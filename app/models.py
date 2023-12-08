81be79e2c11906d931ff08c94437d03ebc9a09df
Ramesh9100 <chitrarameshgowd@gmail.com>
2023-12-08 17:22:12 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Topic(models.Model):
     topic_name=models.CharField(max_length=100,primary_key=True)
 
+    def __str__(self):
+        return self.topic_name
+
 class Webpage(models.Model):
     topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
